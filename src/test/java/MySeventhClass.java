62646655b35648cd4e7ef83680d796d6af2e39f1
unknown <oenalertugrul@gmail.com>
2023-03-14 15:22:29 +0100

    My seventh commir creation is this one

---
+++ src/test/java/MySeventhClass.java
@@ -0,0 +1,9 @@
+import java.sql.SQLOutput;
+
+public class MySeventhClass {
+    public static void main(String[] args) {
+        System.out.println("My seventh commit creation has just been started");
+    }
+
+
+}
